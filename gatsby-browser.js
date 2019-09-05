2a1f455eddfafa77c8193c66fff7fdf09d4c5b8d
Long Nguyen <18nguyenl@Longs-MacBook-Pro.local>
2019-09-05 15:14:59 -0500

    start on lerp scroll effect

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -5,3 +5,11 @@
  */
 
 // You can delete this file if you're not using it
+
+import { map, lerp } from "./src/library/MathUtil"
+
+let winsize;
+const calcWinsize = () => winsize = {width: window.innerWidth, height: window.innerHeight};
+calcWinsize();
+
+window.addEventListener('resize', calcWinsize);
