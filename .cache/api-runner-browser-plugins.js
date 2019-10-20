c63f2069edde7157e9afb64d5aa121240e5b2263
Long Nguyen <18nguyenl@gmail.com>
2019-10-19 20:46:37 -0500

    remove cache

--- .cache/api-runner-browser-plugins.js
+++
@@ -1,7 +0,0 @@
-module.exports = [{
-      plugin: require('/mnt/d/Users/Long/Documents/Projects/long-nguyen-portfolio-2019/node_modules/gatsby-plugin-mdx/gatsby-browser.js'),
-      options: {"plugins":[]},
-    },{
-      plugin: require('/mnt/d/Users/Long/Documents/Projects/long-nguyen-portfolio-2019/gatsby-browser.js'),
-      options: {"plugins":[]},
-    }]
