2fc4359eb1084f33da96bb1a7b36fcfe53ce45de
18nguyenl <18nguyenl@gmail.com>
2019-11-24 16:03:44 -0600

    start switching to barba js

--- _site/_templates/_process/js/script.js
+++ _site/_templates/_process/js/script.js
@@ -7,21 +7,146 @@ import StaggerTranslateUp from './staggerTranslateUp';
 
 import HomeRenderer from './homeRenderer';
 
-window.addEventListener('load', () => {
-    const H = new Highway.Core({
-        transitions: {
-            default: Fade,
-            // blog: StaggerTranslateUp,
-        },
-        renderers: {
-            home: HomeRenderer,
-        }
-    });
+import waveFS from "../../../shaders/wave.frag";
+import waveVS from "../../../shaders/wave.vert";
 
-    H.on('NAVIGATE_IN', () => {
-        // scroll.destroy();
-        // scroll.init();
-        // scroll.scrollTo(scrollContainer)
+window.addEventListener('load', () => {
+    // if ('scrollRestoration' in history) {
+    //     // Back off, browser, I got this...
+    //     history.scrollRestoration = 'manual';
+    // }
+    
+    const menuButton = document.getElementById("hamburger__decoration");
+    const navigation = document.getElementById("navigation");
+    
+    menuButton.addEventListener('click', (e) => {
+        menuButton.classList.toggle('hamburger__menu--exit');
+        navigation.classList.toggle('navigation--visible');
     })
+    
+    const scrollContainer = document.getElementById("js-scroll");
+    
+    const scroll = new LocomotiveScroll({
+        el: scrollContainer,
+        smooth: true,
+        inertia: 0.5,
+        smoothMobile: true,
+    });
+    
+    // scroll.scrollTo(document.querySelector(".portfolio__section"));
+    let planes = [];
+    let scrollEffect = 0;
+    
+    let planeElements = document.getElementsByClassName("plane");
+    
+    const curtains = new Curtains({
+        container: "canvas",
+        watchScroll: false,
+    })
+    
+    curtains.onRender(() => {
+        if(scroll.isMobile) {
+            if(scrollEffect >= 0) {
+                scrollEffect = Math.max(0, scrollEffect - 2);
+            }
+            else {
+                scrollEffect = Math.min(0, scrollEffect + 2);
+            }
+        }
+    })
+    
+    function updateScroll(xOffset, yOffset) {
+        curtains.updateScrollValues(xOffset, yOffset);
+        
+        let delta = curtains.getScrollDeltas();
+        
+        delta.y = -delta.y;
+        
+        // threshold
+        if(delta.y > 60) {
+            delta.y = 60;
+        }
+        else if(delta.y < -60) {
+            delta.y = -60;
+        }
+        
+        if(scroll.isMobile && Math.abs(delta.y) > Math.abs(scrollEffect)) {
+            scrollEffect = delta.y;
+        }
+        else {
+            scrollEffect = delta.y * 1.5;
+        }
+        
+        planes.forEach(e => {
+            e.updateScrollPosition();
+            
+            if (Math.abs(delta.y) <= 1) {
+                e.uniforms.time.value = 0;
+            }
+            
+            e.uniforms.scrollEffect.value = scrollEffect;
+        })
+    }
+    
+    // we'll render only while lerping the scroll
+    // curtains.disableDrawing();
+    scroll.on('scroll', (obj) => {
+        updateScroll(obj.scroll.x, obj.scroll.y);
+    });
+    
+    // no need for shaders as they were already passed by data attributes
+    let params = {
+        vertexShader: waveVS,
+        fragmentShader: waveFS,
+        widthSegments: 10,
+        heightSegments: 10,
+        uniforms: {
+            scrollEffect: {
+                name: "uScrollEffect",
+                type: "1f",
+                value: 0.0,
+            },
+            time: {
+                name: "uTime",
+                type: "1f",
+                value: 0.0,
+            },
+            resolution: {
+                name: "uRes",
+                type: "2f",
+                value: [window.innerWidth, window.innerHeight],
+            }
+        },
+    };
+    
+    // add our planes and handle them
+    for(let i = 0; i < planeElements.length; i++) {
+        let plane = curtains.addPlane(planeElements[i], params);
+        
+        if(plane) {
+            planes.push(plane);
+            
+            handlePlanes(i);
+        }
+    }
+    
+    // handle all the planes
+    function handlePlanes(index) {
+        let plane = planes[index];
+        
+        // check if our plane is defined and use it
+        plane && plane.onReady(function() {
+            // apply parallax on load
+            // applyPlanesParallax(index);
+            
+            // once everything is ready, display everything
+            if(index == planes.length - 1) {
+                document.body.classList.add("planes-loaded");
+                scroll.start();
+            }
+        }).onRender(function() {
+            plane.uniforms.time.value++;
+        })
+    }
 })
 
