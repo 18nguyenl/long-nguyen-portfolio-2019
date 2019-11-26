cb5373a687731513d4a78d0f510a5d6c91820c65
Long Nguyen <18nguyenl@gmail.com>
2019-11-25 20:22:29 -0600

    make page transitions work fluidly

--- _site/_templates/_process/js/script.js
+++ _site/_templates/_process/js/script.js
@@ -1,6 +1,7 @@
 import { Curtains } from 'curtainsjs';
 import LocomotiveScroll from "locomotive-scroll";
 import barba from "@barba/core";
+import barbaPrefetch from '@barba/prefetch';
 
 import Tween from "gsap";
 
@@ -20,85 +21,99 @@ window.addEventListener("load", () => {
   const menuButton = document.getElementById("hamburger__decoration");
   const navigation = document.getElementById("navigation");
 
-  const curtains = new Curtains({
-    container: "canvas",
-    watchScroll: false
-  });
+  AssetLoader.load({}).then(() => {
+    const curtains = new Curtains({
+      container: "canvas",
+      watchScroll: false
+    });
 
-  menuButton.addEventListener("click", e => {
-    menuButton.classList.toggle("hamburger__menu--exit");
-    navigation.classList.toggle("navigation--visible");
-  });
+    menuButton.addEventListener("click", e => {
+      menuButton.classList.toggle("hamburger__menu--exit");
+      navigation.classList.toggle("navigation--visible");
+    });
 
-  const scrollContainer = document.getElementById("js-scroll");
+    const scrollContainer = document.getElementById("js-scroll");
 
-  const scroll = new LocomotiveScroll({
-    el: scrollContainer,
-    smooth: true,
-    inertia: 0.5,
-    smoothMobile: true
-  });
+    const scroll = new LocomotiveScroll({
+      el: scrollContainer,
+      smooth: true,
+      inertia: 0.5,
+      smoothMobile: true
+    });
 
-  const homeShader = new HomeShader(curtains, scroll);
+    const homeShader = new HomeShader(curtains, scroll);
 
-  barba.hooks.before(data => {
-    menuButton.classList.remove("hamburger__menu--exit");
-    navigation.classList.remove("navigation--visible");
-  });
+    barba.hooks.before(data => {
+      menuButton.classList.remove("hamburger__menu--exit");
+      navigation.classList.remove("navigation--visible");
 
-  let fadeTransition = {
-    name: "Fade",
-    leave(data) {
       scroll.destroy();
-      Tween.fromTo(
-        data.current.container,
-        0.5,
-        { opacity: 1 },
-        {
-          opacity: 0,
-        }
-      );
-    },
-    enter(data) {
+    });
+
+    barba.hooks.after(data => {
       scroll.init();
-      // Animation
-      Tween.fromTo(
-        data.next.container,
-        0.5,
-        { opacity: 0 },
-        {
-          opacity: 1,
-        }
-      );
+    })
 
-      AssetLoader.load({
-        element: data.next.container, progress: function (progress, length) {
-          scroll.update();
-          console.log({ progress, length });
-        }
-      }).then(() => {
-        
-      })
-    },
-    after(data) {
-      
-    },
-    namespace: ["home", "blog"]
-  };
+    let fadeTransition = {
+      name: "Fade",
+      before(data) {
+        data.current.namespace == "home" && Tween.fromTo({ scrollEffect: homeShader.scrollEffect }, 0.3, { scrollEffect: 0 }, { scrollEffect: 100 });
+      },
+      leave(data) {
+        Tween.fromTo(
+          data.current.container,
+          0.5,
+          { opacity: 1 },
+          {
+            opacity: 0,
+          }
+        );
+      },
+      enter(data) {
+        // Animation
+        Tween.fromTo(
+          data.next.container,
+          0.5,
+          { opacity: 0 },
+          {
+            opacity: 1,
+          }
+        );
 
-  let homeView = {
-    namespace: "home",
-    afterEnter(data) {
-      homeShader.createPlanes();
-      window.dispatchEvent(new Event('resize'));
-    },
-    beforeLeave(data) {
-      homeShader.cleanPlanes();
-    },
-  };
+        AssetLoader.load({
+          element: data.next.container, progress: function (progress, length) {
+            console.log({ progress, length });
+          }
+        }).then(() => {
+        })
+      },
+      after(data) {
+      },
+      namespace: ["home", "blog"]
+    };
 
-  barba.init({
-    transitions: [fadeTransition],
-    views: [homeView]
-  });
+    let homeView = {
+      namespace: "home",
+      beforeEnter(data) {
+        AssetLoader.load({
+          element: data.next.container, progress: function (progress, length) {
+            console.log({ progress, length });
+          }
+        }).then(() => {
+          homeShader.createPlanes();
+          console.log("planes drawn")
+        })
+      },
+      beforeLeave(data) {
+        homeShader.cleanPlanes();
+      }
+    }
+
+    barba.use(barbaPrefetch);
+
+    barba.init({
+      transitions: [fadeTransition],
+      views: [homeView]
+    });
+  })
 });
