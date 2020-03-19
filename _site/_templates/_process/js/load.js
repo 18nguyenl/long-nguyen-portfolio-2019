d53f8548da81237d7ca822517342bdcfe09f92ba
Long Nguyen <18nguyenl@gmail.com>
2020-03-19 14:46:19 -0500

    style post subpage, fix mobile scrolling bug

--- _site/_templates/_process/js/load.js
+++ _site/_templates/_process/js/load.js
@@ -2,9 +2,9 @@ import isMobile from './utils/touchscreen';
 
 class Load {
     constructor() {
-        document.body.style.overflow = "hidden";
-
         if (!isMobile()) {
+            document.body.style.overflow = "hidden";
+
             document.querySelectorAll('a').forEach((ele) => {
                 ele.addEventListener("click", (e) => {
                     e.preventDefault();
